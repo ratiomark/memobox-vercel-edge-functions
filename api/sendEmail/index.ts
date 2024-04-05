a284bb44ca2120ba198d9278f733a2cc0b2c71d9
ratiomark <yanagae@gmail.com>
2024-04-05 18:37:48 +0300

    add: send email function

---
+++ api/sendEmail/index.ts
@@ -0,0 +1,118 @@
+import sgMail, { MailDataRequired } from '@sendgrid/mail'
+type language = 'en' | 'ru'
+const languages: language[] = ['en', 'ru']
+
+interface EmailParams {
+	to: string
+	emailType: string
+	language: language
+	data?: any
+}
+
+interface FromObject {
+	email: string
+	name: string
+}
+
+interface SendGridData extends FromObject {
+	templateId: string
+	subject: string
+}
+
+async function getBackendUrl() {
+	const backendUrl = process.env.BACKEND_URL
+	if (!backendUrl) {
+		return 'https://memobox.tech/'
+	}
+	return backendUrl
+}
+
+// '{"name": "Welcome to memobox!", "email": "just@@memobox.tech", "templateId":" d-dd2231c4f21347aa9be47af870de084c", "subject": "Welcome to memobox!"}'
+// async function getSendGridDataByLangAndType(language: string, emailType: string) {
+// 	const command = new GetParameterCommand({
+// 		Name: `/SendGridData/${language}/${emailType}`,
+// 		WithDecryption: true,
+// 	})
+// 	const response = await client.send(command)
+// 	if (!response.Parameter?.Value) {
+// 		throw new Error('SendGrid template id not found')
+// 	}
+// 	return JSON.parse(response.Parameter.Value) as SendGridData
+// }
+async function getSendGridDataByLangAndType(language: language, emailType: string) {
+	let data = process.env[`SEND_GRID_DATA_${language.toUpperCase()}_${emailType}`]
+	if (!data) {
+		data = process.env[`SEND_GRID_DATA_EN_${emailType}`]
+	}
+	return JSON.parse(data!) as SendGridData
+}
+// {"name": "Welcome to memobox!", "email": "just@memobox.tech", "templateId":" d-dd2231c4f21347aa9be47af870de084c", "subject": "registration"}
+// async function getSendGridData(language: string, emailType: string): SendGridData {
+// 	const apiKey = await getSendGridApiKey()
+// 	const templateId = await getSendGridTemplateId(language, emailType)
+// 	const from = {
+// 		email: '
+// }
+
+export const handler = async (event: { body: EmailParams }) => {
+	try {
+		console.log('Received event:', event)
+		if (!event.body) {
+			console.error('No body found in event')
+			return { statusCode: 400, body: 'No body found in event' }
+		}
+		if (!event.body.language) {
+			event.body.language = 'en'
+		}
+		if (!event.body.to) {
+			console.error('No recipient found in body')
+			return { statusCode: 400, body: 'No recipient found in body - field with name "to" is undefined' }
+		}
+		if (!event.body.emailType) {
+			console.error('No emailType found in body')
+			return { statusCode: 400, body: 'No emailType found in body' }
+		}
+
+		const sendGridApiKey = process.env.SEND_GRID_API_KEY!
+		sgMail.setApiKey(sendGridApiKey)
+
+		let sendGridData = await getSendGridDataByLangAndType(event.body.language, event.body.emailType)
+		if (!sendGridData) {
+			console.error('No sendGridData found')
+			sendGridData = await getSendGridDataByLangAndType('en', event.body.emailType)
+			if (!sendGridData) {
+				console.error('No sendGridData found')
+				return { statusCode: 400, body: 'No sendGridData found' }
+			}
+		}
+
+		const { name, email, templateId, subject } = sendGridData
+
+		console.log('dataTest:', sendGridData)
+
+		const { to } = event.body
+
+		const msg: MailDataRequired = {
+			from: {
+				email,
+				name: 'Memobox',
+			},
+			personalizations: [
+				{
+					to: [{ email: to }],
+					dynamicTemplateData: {
+						...event.body.data,
+						subject,
+					},
+				},
+			],
+			templateId,
+		}
+
+		const [response] = await sgMail.send(msg)
+		return { statusCode: 200, body: response }
+	} catch (error) {
+		console.error(error)
+		return { statusCode: 500, body: 'Failed to send email' }
+	}
+}
